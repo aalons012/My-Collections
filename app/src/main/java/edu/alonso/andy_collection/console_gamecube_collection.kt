91985a2701cb43545314c67fa003adc0ca092dc1
Andy Alonso <andyalonso281@gmail.com>
2025-06-30 17:03:10 -0400

    imma be honest i dont remember

--- app/src/main/java/edu/alonso/andy_collection/console_gamecube_collection.kt
+++ app/src/main/java/edu/alonso/andy_collection/console_gamecube_collection.kt
@@ -7,11 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class console_collection : AppCompatActivity() {
+class console_gamecube_collection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_console_collection)
+        setContentView(R.layout.activity_console_gamecube_collection)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -19,24 +19,13 @@ class console_collection : AppCompatActivity() {
         }
     }
 
+    fun onClickPokemon(view: View) {}
+    fun onClickssbm(view: View) {}
+    fun onClickX(view: View) {}
+    fun onClickMkdd(view: View) {}
+    fun onClickZelda(view: View) {}
     fun onClickBack(view: View) {
         finish()
-    }
-
-    fun onClickWii(view: View) {
 
     }
-    fun onClickPS(view: View) {
-
-    }
-    fun onClickSwitch(view: View) {
-
-    }
-    fun onClickSteamdeck(view: View) {
-
-    }
-    fun onClickGamecube(view: View) {
-        
-    }
-
 }
