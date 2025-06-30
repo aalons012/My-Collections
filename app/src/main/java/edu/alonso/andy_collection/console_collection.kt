ac969a2109caa8f26872d8856104e97c5b454630
Andy Alonso <andyalonso281@gmail.com>
2025-06-30 13:06:05 -0400

    edit button programming; added console collection activity

--- app/src/main/java/edu/alonso/andy_collection/console_collection.kt
+++ app/src/main/java/edu/alonso/andy_collection/console_collection.kt
@@ -1,27 +1,20 @@
 package edu.alonso.andy_collection
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class consoles : AppCompatActivity() {
+class console_collection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_consoles)
+        setContentView(R.layout.activity_console_collection)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-
-
-    fun onClickBack(view: View) {
-        finish()
     }
 }
--- app/src/main/java/edu/alonso/andy_collection/game_console_option.kt
+++ app/src/main/java/edu/alonso/andy_collection/game_console_option.kt
@@ -1,6 +1,5 @@
 package edu.alonso.andy_collection
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
@@ -8,11 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class game_console_option : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_consoles_game_options)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -20,15 +19,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onClickConsoles(view: View) {
 
-    }
-    fun onClickGames(view: View) {
-        startActivity(Intent(this, game_console_option::class.java))
-        startActivity(intent)
-    }
-    fun onClickAbout(view: View) {
-        startActivity(Intent(this, About::class.java))
-        startActivity(intent)
+
+    fun onClickBack(view: View) {
+        finish()
     }
 }
