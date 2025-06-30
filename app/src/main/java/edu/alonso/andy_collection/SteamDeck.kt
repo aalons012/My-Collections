327bfdb41ce81aa7ebd1d55620aa20f99a43fc28
Andy Alonso <andyalonso281@gmail.com>
2025-06-30 18:55:02 -0400

    adding code

--- app/src/main/java/edu/alonso/andy_collection/SteamDeck.kt
+++ app/src/main/java/edu/alonso/andy_collection/SteamDeck.kt
@@ -1,6 +1,7 @@
 package edu.alonso.andy_collection
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,9 @@ class SteamDeck : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun onCslickBack(view: View) {
+        finish()
     }
 }
